439d89806f6d2e62adb6352127324189f851221f
= <ronaldinhofarfan@gmail.com>
2021-05-25 23:20:34 -0500

    oks

---
+++ curso_poo/prueba.py
@@ -0,0 +1,8 @@
+class Auto:
+    def __init__(self,marca='default',modelo='default',placa='000000'):
+        print(f'Propiedades de coche')
+        self.marca = marca
+        self.modelo = modelo
+        self.placa = placa
+mi_coche = Auto('Renauld','xSD5s','g5xxd')
+print(mi_coche.placa)
